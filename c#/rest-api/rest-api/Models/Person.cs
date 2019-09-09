2a780b3a56421a66bb27040ca002cae84d7fd18e
Inacio Klassmann <inacio.klassmann@ilegra.com>
2019-09-08 21:53:05 -0300

    first commit

---
+++ c#/rest-api/rest-api/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace rest_api.Models
+{
+    public class Person
+    {
+        String name;
+        int age;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
