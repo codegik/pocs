b5ba9704e160e031819f0582c80bd7d97941536a
Inacio Klassmann <inaciosk8@gmail.com>
2021-04-09 14:59:31 -0300

    added spring-boot-hello for testing pourpose

---
+++ devops/microvm/spring-boot-hello/src/test/java/com/codegik/hello/SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegik.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
