ecfbbd4b57080ad2f6b12bce829bb87544bda655
Inacio Klassmann <iklassman@lendingclub.com>
2022-07-04 14:05:15 -0300

    WIP: adding kotlin-resilience4j poc

---
+++ kotlin-resilience4j/product/src/main/kotlin/com/codegik/poc/javaresilience4j/domain/Chair.java
@@ -0,0 +1,47 @@
+package com.codegik.poc.javaresilience4j.domain;
+
+public class Chair {
+    private String id;
+    private String name;
+    private int width;
+    private int height;
+
+    public Chair(String id, String name, int width, int height) {
+        this.id = id;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
