7ef448b43c979807a088f4fa85f026054e54cd27
Inacio Klassmann <iklassman@lendingclub.com>
2022-07-12 13:48:13 -0300

    adding github-client poc

--- kotlin-github-api/src/main/kotlin/com/codegik/poc/github/App.kt
+++ kotlin-github-api/src/main/kotlin/com/codegik/poc/github/App.kt
@@ -1,16 +1,24 @@
 package com.codegik.poc.github
 
+import com.codegik.poc.github.service.GithubService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class App : CommandLineRunner {
-    override fun run(vararg args: String?) {
 
+    @Autowired
+    private lateinit var githubService: GithubService
+
+    override fun run(vararg args: String?) {
+        githubService.findLastWeekPRs()
+        exitProcess(0)
     }
 }
 
 fun main(args: Array<String>) {
-    runApplication<App>(*args);
+    runApplication<App>(*args).close();
 }
--- kotlin-github-api/src/main/kotlin/com/codegik/poc/github/service/GithubService.kt
+++ kotlin-github-api/src/main/kotlin/com/codegik/poc/github/service/GithubService.kt
@@ -1,19 +1,58 @@
 package com.codegik.poc.github.service
 
 import com.spotify.github.v3.clients.GitHubClient
+import com.spotify.github.v3.prs.requests.ImmutablePullRequestParameters
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.net.URI
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalAmount
+import java.time.temporal.TemporalUnit
 
 
 @Service
 class GithubService {
     private val logger = LoggerFactory.getLogger(GithubService::class.java)
 
+    @Value("\${github.uri:https://github.tlcinternal.com/api/v3}")
+    private lateinit var uri: String
 
-    fun getPRs() {
-        val githubClient = GitHubClient.create(URI.create("https://api.github.com/"),"")
+    @Value("\${github.user:username}")
+    private lateinit var user: String
+
+    @Value("\${github.accessToken:hashtoken}")
+    private lateinit var accessToken: String
+
+    @Value("\${github.repositories}")
+    private lateinit var repositories: List<String>
 
 
+    fun findLastWeekPRs() {
+        var prsStr = ""
+        val sevenDaysAgo = Instant.now().minus(7, ChronoUnit.DAYS);
+        val closedFilter = ImmutablePullRequestParameters.builder().state("closed").build()
+
+        repositories.forEach { repository ->
+            val (org, repo) = repository.split("/").let { Pair(it[0], it[1]) }
+            val prClient = GitHubClient.create(URI.create(uri), accessToken)
+                .createRepositoryClient(org, repo)
+                .createPullRequestClient()
+
+            // finding closed PRs
+            prClient.list(closedFilter).join()
+                .filter { it.user()?.login().equals(user) }
+                .filter { it.createdAt()?.instant()?.isAfter(sevenDaysAgo) == true }
+                .forEach { prsStr += "\n${it.htmlUrl()}" }
+
+            // finding open PRs
+            prClient.list().join()
+                .filter { it.user()?.login().equals(user) }
+                .filter { it.createdAt()?.instant()?.isAfter(sevenDaysAgo) == true }
+                .forEach { prsStr += "\n${it.htmlUrl()}" }
+        }
+
+        logger.info("Last Week PRs from {} {}", user, prsStr)
     }
 }
