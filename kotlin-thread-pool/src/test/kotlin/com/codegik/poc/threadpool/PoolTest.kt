db848d58368c24f54c7f78472a9d73f44b8a368a
Inacio Klassmann <iklassman@lendingclub.com>
2022-08-10 09:08:34 -0300

    removed smart thread and keep thread pool

--- kotlin-thread-pool/src/test/kotlin/com/codegik/poc/threadpool/PoolTest.kt
+++ kotlin-thread-pool/src/test/kotlin/com/codegik/poc/threadpool/PoolTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 
 class PoolTest {
 
-	private val repeatTaskCreation = 3000
 	private val timeOutList = listOf(
 		10000L, 4000L, 1000L, 200L, 200L, 100L, 100L, 100L, 100L, 50L,
 		5L, 50L, 4000L, 50L, 50L, 1000L, 50L, 50L, 5L, 10000L
@@ -14,11 +13,11 @@ class PoolTest {
 
 
 	@Test
-	fun threadPoolRunTasks() {
+	fun run60kTasks() {
 		val threadPool = ThreadPool(1000)
 		var taskCount = 0
 
-		for (i in 0..repeatTaskCreation) {
+		for (i in 0..3000) {
 			for (j in 0 until timeOutList.size) {
 				threadPool.addTask(DelayTask("$taskCount", timeOutList[j]))
 				taskCount++
@@ -27,5 +26,16 @@ class PoolTest {
 
 		val threadPoolTook = threadPool.waitToFinish()
 		println("ThreadPool took $threadPoolTook ms to execute $taskCount tasks")
+	}
+
+	@Test
+	fun runLongTasks() {
+		val threadPool = ThreadPool(100)
+
+		for (i in 1..10) {
+			threadPool.addTask(DelayTask("delay-task-$i", 40000))
+		}
+
+		threadPool.waitToFinish()
 	}
 }
