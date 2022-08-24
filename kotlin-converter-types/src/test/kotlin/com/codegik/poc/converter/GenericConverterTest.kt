8e761fb6d42d2924c30f38b59978dbc58e2d7053
Inacio Klassmann <iklassman@lendingclub.com>
2022-08-24 18:34:10 -0300

    added generic converter

--- kotlin-converter-types/src/test/kotlin/com/codegik/poc/converter/GenericConverterTest.kt
+++ kotlin-converter-types/src/test/kotlin/com/codegik/poc/converter/GenericConverterTest.kt
@@ -11,9 +11,17 @@ class GenericConverterTest {
 	@Test
 	fun convertGeneric() {
 		val converter = GenericConverter()
-		val person = Person("Inacio", 38)
-		val friend = converter.from(person).to(Friend::class.java)
+		val person1 = Person("Inacio", 38)
+		val friend1 = converter.from(person1).to(Friend::class.java)
 
-		assertEquals(person.name, friend.name)
+		assertEquals(person1.name, friend1.name)
+		assertEquals(person1.age, friend1.age)
+
+		val friend2 = Friend("Diego", 55, "email@gmail.com")
+		val person2 = converter.from(friend2).to(Person::class.java)
+
+		assertEquals(person2.name, friend2.name)
+		assertEquals(person2.age, friend2.age)
+		assertEquals("email@gmail.com", friend2.email)
 	}
 }
