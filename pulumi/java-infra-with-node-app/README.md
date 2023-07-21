4b4259d799af662e6fd7a59ad2d7d2820ce98ad2
Inacio Klassmann <iklassman@lendingclub.com>
2023-07-21 14:42:10 -0300

    adding pulumi pocs

--- pulumi/java-infra-with-node-app/README.md
+++ pulumi/java-infra-with-node-app/README.md
@@ -7,4 +7,28 @@ This is an POC to setup infrastructure managed by java, and deploy node applicat
 - Java 17
 - Gradle
 - Docker
-- 
+
+## Setup
+
+For MacOS users, the docker host address might be different. In my local machine, the docker host is:
+```
+unix:///Users/iklassman/.docker/run/docker.sock
+```
+
+So its necessary to configure docker host in pulumi running these commands:
+```
+export DOCKER_HOST=unix:///Users/iklassman/.docker/run/docker.sock
+
+pulumi config set docker:host unix:///Users/iklassman/.docker/run/docker.sock
+```
+
+Configuring the containers
+```
+pulumi config set frontendPort 3001
+pulumi config set backendPort 3000
+pulumi config set mongoPort 27017
+pulumi config set mongoHost mongodb://mongo:27017
+pulumi config set database cart
+pulumi config set nodeEnvironment development
+pulumi config set protocol http://
+```
--- pulumi/java-infra-with-node-app/app/src/main/java/myproject/App.java
+++ pulumi/java-infra-with-node-app/app/src/main/java/myproject/App.java
@@ -2,8 +2,18 @@ package myproject;
 
 import com.pulumi.Context;
 import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.docker.Container;
+import com.pulumi.docker.ContainerArgs;
+import com.pulumi.docker.Network;
+import com.pulumi.docker.NetworkArgs;
 import com.pulumi.docker.RemoteImage;
 import com.pulumi.docker.RemoteImageArgs;
+import com.pulumi.docker.inputs.ContainerNetworksAdvancedArgs;
+import com.pulumi.docker.inputs.ContainerPortArgs;
+import com.pulumi.resources.CustomResourceOptions;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +21,15 @@ public class App {
     }
 
     private static void stack(Context ctx) {
+        // Get configuration values
+        final var config = ctx.config();
+        final var frontendPort = config.requireInteger("frontendPort");
+        final var backendPort = config.requireInteger("backendPort");
+        final var mongoPort = config.requireInteger("mongoPort");
+        final var mongoHost = config.require("mongoHost");
+        final var database = config.require("database");
+        final var nodeEnvironment = config.require("nodeEnvironment");
+        final var protocol = config.require("protocol");
 
         final var stackName = ctx.stackName();
 
@@ -22,5 +41,96 @@ public class App {
                         .name(String.format("pulumi/tutorial-pulumi-fundamentals-%s:latest", backendImageName))
                         .build()
         );
+
+        // Pull the frontend image
+        final String frontendImageName = "frontend";
+        final var frontendImage = new RemoteImage(
+                frontendImageName,
+                RemoteImageArgs.builder()
+                        .name(String.format("pulumi/tutorial-pulumi-fundamentals-%s:latest", frontendImageName))
+                        .build()
+        );
+
+        // Pull the MongoDB image
+        final var mongoImage = new RemoteImage(
+                "mongoImage",
+                RemoteImageArgs.builder()
+                        .name("pulumi/tutorial-pulumi-fundamentals-database-local:latest")
+                        .build()
+        );
+
+        // Create a Docker Network
+        final var network = new Network(
+                "network",
+                NetworkArgs.builder()
+                        .name(String.format("services-%s", stackName))
+                        .build()
+        );
+
+        // Create the MongoDB container
+        final var mongoContainer = new Container(
+                "mongoContainer",
+                ContainerArgs.builder()
+                        .name(String.format("mongo-%s", stackName))
+                        .image(mongoImage.repoDigest())
+                        .ports(ContainerPortArgs.builder()
+                                .internal(mongoPort)
+                                .external(mongoPort)
+                                .build())
+                        .networksAdvanced(ContainerNetworksAdvancedArgs.builder()
+                                .name(network.name())
+                                .aliases("mongo")
+                                .build()
+                        )
+                        .build()
+        );
+
+        // Create the frontend container
+        final var frontendContainer = new Container(
+                "frontendContainer",
+                ContainerArgs.builder()
+                        .name(String.format("frontend-%s", stackName))
+                        .image(frontendImage.repoDigest())
+                        .ports(ContainerPortArgs.builder()
+                                .internal(frontendPort)
+                                .external(frontendPort)
+                                .build())
+                        .envs(List.of(
+                                String.format("PORT=%d", frontendPort),
+                                String.format("HTTP_PROXY=backend-%s:%d", stackName, backendPort),
+                                String.format("PROXY_PROTOCOL=%s", protocol)
+                        ))
+                        .networksAdvanced(ContainerNetworksAdvancedArgs.builder()
+                                .name(network.name())
+                                .build())
+                        .build()
+        );
+
+        // Create the backend container
+        final var backendContainer = new Container(
+                "backendContainer",
+                ContainerArgs.builder()
+                        .name(String.format("backend-%s", stackName))
+                        .image(backendImage.repoDigest())
+                        .ports(ContainerPortArgs.builder()
+                                .internal(backendPort)
+                                .external(backendPort)
+                                .build())
+                        .envs(List.of(
+                                String.format("DATABASE_HOST=%s", mongoHost),
+                                String.format("DATABASE_NAME=%s", database),
+                                String.format("NODE_ENV=%s", nodeEnvironment)
+                        ))
+                        .networksAdvanced(ContainerNetworksAdvancedArgs.builder()
+                                .name(network.name())
+                                .build()
+                        )
+                        .build(),
+                CustomResourceOptions.builder()
+                        .dependsOn(mongoContainer)
+                        .build()
+        );
+
+        ctx.export("link", Output.of("http://localhost:3001"));
     }
 }
