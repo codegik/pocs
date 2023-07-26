e2e327cd762279520087bd50aea026feac19b342
Inacio Klassmann <iklassman@lendingclub.com>
2023-07-26 14:43:39 -0300

    adding pulumi pocs

---
+++ pulumi/ts-container/index.ts
@@ -0,0 +1,41 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// An ECS cluster to deploy into.
+const cluster = new aws.ecs.Cluster("cluster", {});
+
+// An ALB to serve the container endpoint to the internet.
+const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {});
+
+// An ECR repository to store our application's container image.
+const repo = new awsx.ecr.Repository("repo", {
+    forceDelete: true,
+});
+
+// Build and publish our application's container image from ./app to the ECR repository.
+const image = new awsx.ecr.Image("image", {
+    repositoryUrl: repo.url,
+    path: "./app",
+});
+
+// Deploy an ECS Service on Fargate to host the application container.
+const service = new awsx.ecs.FargateService("service", {
+    cluster: cluster.arn,
+    assignPublicIp: true,
+    taskDefinitionArgs: {
+        container: {
+            image: image.imageUri,
+            cpu: 128,
+            memory: 512,
+            essential: true,
+            portMappings: [{
+                containerPort: 80,
+                targetGroup: loadbalancer.defaultTargetGroup,
+            }],
+        },
+    },
+});
+
+// The URL at which the container's HTTP endpoint will be available.
+export const frontendURL = pulumi.interpolate`http://${loadbalancer.loadBalancer.dnsName}`;
