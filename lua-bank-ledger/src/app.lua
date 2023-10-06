972c8ebc0187b34e63446e2888db5c6d92db0c08
Inacio Klassmann <iklassman@lendingclub.com>
2023-10-06 13:53:45 -0300

    adding lua bank ledger poc

--- lua-bank-ledger/src/app.lua
+++ lua-bank-ledger/src/app.lua
@@ -1,14 +1,3 @@
 require("src/account")
-require("src/transaction")
-require("src/ledger")
 
 print(Account("4444", 9).getBalance())
-
-local account1 = Account("1234", 0)
-local account2 = Account("5678", 10)
-local ledger = Ledger()
-
-ledger.process(Transaction(account2, account1, 5))
-ledger.process(Transaction(account2, account1, 2))
-ledger.process(Transaction(account1, account2, 7))
-ledger.restore()
