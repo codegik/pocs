1931c65c62d6316357188d7749c0f0ec993e93cd
Inacio Klassmann <iklassman@lendingclub.com>
2023-11-01 15:16:12 -0300

    adding kotlin jdk21 dontpad poc

--- kotlin-jdk21-dontpad/src/main/kotlin/com/codegik/dontpad/controller/DontpadView.kt
+++ kotlin-jdk21-dontpad/src/main/kotlin/com/codegik/dontpad/controller/DontpadView.kt
@@ -1,34 +1,34 @@
 package com.codegik.dontpad.controller
 
-import org.springframework.http.MediaType
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.ui.set
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 data class Todo(val id: Int, val title: String)
 
 @Controller
-@RequestMapping("/todos")
+@RequestMapping("/")
 class DontpadView {
 
-    private var todos = mutableListOf(Todo(3, "Test"), Todo(4, "new one"))
-
-
     @GetMapping
-    fun getAll(model: Model): String {
-        model["todos"] = todos
-        return "todos"
+    fun index(): String {
+        return "index"
     }
 
 
-    @DeleteMapping("/{id}", produces = [MediaType.TEXT_HTML_VALUE])
-    fun delete(@PathVariable id: Int): String? {
-        todos = todos.dropWhile { it.id == id }.toMutableList()
-        println("Deleted $id")
-        return null
+    @GetMapping("/dontpad/{dontpad-name}")
+    fun dontpad(@PathVariable("dontpad-name") dontpadName: String): String {
+        return "dontpad"
+    }
+
+
+    @PostMapping("/dontpad")
+    fun create(@RequestParam("dontpad-name") dontpadName: String, response: HttpServletResponse): String {
+        response.addHeader("HX-Redirect", "/dontpad/$dontpadName")
+        return "";
     }
 }
