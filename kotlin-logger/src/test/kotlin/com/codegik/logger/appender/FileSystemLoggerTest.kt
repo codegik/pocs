a60bf3225ca2608dabe0538d64d582bd7ebdd4d6
Inacio Klassmann <iklassman@lendingclub.com>
2024-02-15 15:26:19 -0300

    adding kotlin logger api

--- kotlin-logger/src/test/kotlin/com/codegik/logger/appender/FileSystemLoggerTest.kt
+++ kotlin-logger/src/test/kotlin/com/codegik/logger/appender/FileSystemLoggerTest.kt
@@ -1,17 +1,28 @@
 package com.codegik.logger.appender
 
 import com.codegik.logger.config.LoggerConfig
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.lang.Thread.sleep
 
 class FileSystemLoggerTest {
 
 	@Test
-	fun teste1() {
+	fun messagesShouldBePresentInFile() {
 		val fileName = LoggerConfig.fileName()
-		val logger = FileSystemLogger(FileSystemLoggerTest::class.java)
+		val logger1 = FileSystemLogger(FileSystemLoggerTest::class.java)
+		val logger2 = FileSystemLogger(LoggerConfig::class.java)
 
-		logger.info("first message")
+		logger1.info("first message")
+		logger2.info("second message")
+
+		sleep(100)
+
+		val content = File(fileName).readText(Charsets.UTF_8)
+
+		assertTrue(content.contains("[INFO] [com.codegik.logger.appender.FileSystemLoggerTest] first message"))
+		assertTrue(content.contains("[INFO] [com.codegik.logger.config.LoggerConfig] second message"))
 
 		File(fileName).delete()
 	}
