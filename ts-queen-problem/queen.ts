8c563a8ae85561f9916badbfe44e65647a41ebd9
Inacio Klassmann <inacioklassmann@gmail.com>
2024-07-05 10:37:46 -0300

    Revert "updating ts queen problem"
    This reverts commit f007d04e7055d3792349cc6e1761c2e329bb1293.

--- ts-queen-problem/queen.ts
+++ ts-queen-problem/queen.ts
@@ -1,5 +1,5 @@
 export class QueenProblem {
-  boardSize = 8;
+  boardSize = 4;
 
   boardAsString(board: number[][]): String {
     let result = "";
@@ -55,14 +55,10 @@ export class QueenProblem {
 
   solve(): String {
     let board = [
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
     ];
 
     if (this.search(board, 0) == false) {
