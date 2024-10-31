74c99de1a80c650b3190656122114fe2176b14bb
Inacio Klassmann <inacioklassmann@gmail.com>
2024-10-31 13:11:26 -0300

    adding vector databases poc

--- vectordb/chromadb/app.py
+++ vectordb/chromadb/app.py
@@ -2,26 +2,17 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 
 student_info = """
-Alexandra Thompson, a 19-year-old computer science sophomore with a 3.7 GPA,
-is a member of the programming and chess clubs who enjoys pizza, swimming, and hiking
-in her free time in hopes of working at a tech company after graduating from the University of Washington.
+Alexandra Thompson, a 19-year-old computer science sophomore with a 3.7 GPA
 """
 
 club_info = """
 The university chess club provides an outlet for students to come together and enjoy playing
-the classic strategy game of chess. Members of all skill levels are welcome, from beginners learning
-the rules to experienced tournament players. The club typically meets a few times per week to play casual games,
-participate in tournaments, analyze famous chess matches, and improve members' skills.
+the classic strategy game of chess
 """
 
 university_info = """
-The University of Washington, founded in 1861 in Seattle, is a public research university
-with over 45,000 students across three campuses in Seattle, Tacoma, and Bothell.
-As the flagship institution of the six public universities in Washington state,
-UW encompasses over 500 buildings and 20 million square feet of space,
-including one of the largest library systems in the world.
+The University of Washington, founded in 1861 in Seattle
 """
-
 
 client = chromadb.HttpClient(host='localhost', port=8000)
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
