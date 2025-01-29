5cb88d76cec6e3a536ab820b4c2168c511c0929e
Inacio Klassmann <inacioklassmann@gmail.com>
2025-01-28 21:56:14 -0300

    adding terraform gcp poc

--- terraform-gcp/cloud-function/provider.tf
+++ terraform-gcp/cloud-function/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.34.0"
+    }
+  }
+}
+
 provider "google" {
   project     = "pristine-bay-448712-v3"
   region      = "us-central1"
