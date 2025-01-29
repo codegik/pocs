1a37f78b355c3019019cb6108e277b6c86d46c28
Inacio Klassmann <inacioklassmann@gmail.com>
2025-01-28 23:28:22 -0300

    adding terraform gcp poc

--- terraform-gcp/cloud-function/main.tf
+++ terraform-gcp/cloud-function/main.tf
@@ -1,16 +1,3 @@
-#resource "google_compute_network" "vpc_network" {
-#  name                    = "my-custom-mode-network"
-#  auto_create_subnetworks = false
-#  mtu                     = 1460
-#}
-#
-#resource "google_compute_subnetwork" "default" {
-#  name          = "my-custom-subnet"
-#  ip_cidr_range = "10.0.1.0/24"
-#  region        = "us-west1"
-#  network       = google_compute_network.vpc_network.id
-#}
-#
 
 resource "google_storage_bucket" "bucket" {
   name                        = "codegik-bucket"
