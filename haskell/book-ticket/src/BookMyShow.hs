70390bb2ca61492a1a74e1622e9f9c8d19c76bed
Inacio Klassmann <inacioklassmann@gmail.com>
2025-04-14 08:39:18 -0700

    adding haskell poc

--- haskell/book-ticket/src/BookMyShow.hs
+++ haskell/book-ticket/src/BookMyShow.hs
@@ -89,15 +89,3 @@ scatter bookMyShow slots maxRow = do
     when success $ writeIORef (startRow bookMyShow) newStart
     return success
 
--- main :: IO ()
--- main = do
---     bookShow <- newBookMyShow 2 5
---     gatherResult1 <- gather bookShow 4 0
---     putStrLn $ "gather 4 0: " ++ show gatherResult1
---     gatherResult2 <- gather bookShow 2 0
---     putStrLn $ "gather 2 0: " ++ show gatherResult2
---     scatterResult1 <- scatter bookShow 5 1
---     putStrLn $ "scatter 5 1: " ++ show scatterResult1
---     scatterResult2 <- scatter bookShow 5 1
---     putStrLn $ "scatter 5 1: " ++ show scatterResult2
-
